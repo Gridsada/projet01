2cdf3da2357706c9454a1f33f2e186ee62fb1551
gridsada <gridsada.simma@gmail.com>
2016-11-08 18:01:05 +0700

    add projet member crud

---
+++ application/controllers/Person.php
@@ -0,0 +1,130 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Person extends CI_Controller {
+
+	public function __construct()
+	{
+		parent::__construct();
+		$this->load->model('person_model','person');
+	}
+
+	public function index()
+	{
+		$this->load->helper('url');
+		$this->load->view('person_view');
+	}
+
+	public function ajax_list()
+	{
+		$list = $this->person->get_datatables();
+		$data = array();
+		$no = $_POST['start'];
+		
+
+		foreach ($list as $person) {
+			$no++;
+			$row = array();
+			//$row[] = $person->id;
+			$row[] = $person->firstName;
+			$row[] = $person->lastName;
+			$row[] = $person->age;
+	
+
+			//add html for action
+			$row[] = '<a class="btn btn-sm btn-primary" href="javascript:void(0)" title="Edit" onclick="edit_person('."'".$person->id."'".')"><i class="glyphicon glyphicon-pencil"></i>แก้ไข</a>
+				  <a class="btn btn-sm btn-danger" href="javascript:void(0)" title="Hapus" onclick="delete_person('."'".$person->id."'".')"><i class="glyphicon glyphicon-trash"></i> ลบ</a>';
+		
+			$data[] = $row;
+		}
+
+		$output = array(
+						"draw" => $_POST['draw'],
+						"recordsTotal" => $this->person->count_all(),
+						"recordsFiltered" => $this->person->count_filtered(),
+						"data" => $data,
+				);
+		//output to json format
+		echo json_encode($output);
+	}
+
+	public function ajax_edit($id)
+	{
+		$data = $this->person->get_by_id($id);
+		
+		echo json_encode($data);
+	}
+
+	public function ajax_add()
+	{
+		$this->_validate();
+		$data = array(
+				'firstName' => $this->input->post('firstName'),
+				'lastName' => $this->input->post('lastName'),
+				'age' => $this->input->post('age'),
+				
+			);
+		$insert = $this->person->save($data);
+		echo json_encode(array("status" => TRUE));
+	}
+
+	public function ajax_update()
+	{
+		$this->_validate();
+		$data = array(
+				'firstName' => $this->input->post('firstName'),
+				'lastName' => $this->input->post('lastName'),
+				'age' => $this->input->post('age'),
+				
+			);
+		$this->person->update(array('id' => $this->input->post('id')), $data);
+		echo json_encode(array("status" => TRUE));
+	}
+
+	public function ajax_delete($id)
+	{
+		$this->person->delete_by_id($id);
+		echo json_encode(array("status" => TRUE));
+	}
+
+
+	private function _validate()
+	{
+		$data = array();
+		$data['error_string'] = array();
+		$data['inputerror'] = array();
+		$data['status'] = TRUE;
+
+		if($this->input->post('firstName') == '')
+		{
+			$data['inputerror'][] = 'firstName';
+			$data['error_string'][] = 'โปรดกรอกชื่อ';
+			$data['status'] = FALSE;
+		}
+
+		if($this->input->post('lastName') == '')
+		{
+			$data['inputerror'][] = 'lastName';
+			$data['error_string'][] = 'โปรดกรอกนามสกุล';
+			$data['status'] = FALSE;
+		}
+
+
+
+		if($this->input->post('age') == '')
+		{
+			$data['inputerror'][] = 'age';
+			$data['error_string'][] = 'โปรดกรอกอายุ';
+			$data['status'] = FALSE;
+		}
+
+		
+
+		if($data['status'] === FALSE)
+		{
+			echo json_encode($data);
+			exit();
+		}
+	}
+
+}
